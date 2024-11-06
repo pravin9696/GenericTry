5d8d582e10b0f62f9dec30b1c848327e7e91f1dd
Acer <Acer@DESKTOP-ABKHEEV>
2024-11-06 11:47:42 +0530

    Add project files.

---
+++ GenericTry/Generic collection example/ListDemo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTry.Generic_collection_example
+{
+    internal class ListDemo
+    {
+        public void SortedDictionaryDemo()
+        {
+            SortedDictionary<string, int> abc = new SortedDictionary<string, int>();
+            abc.Add("b", 1);
+                abc.Add("a", 2);
+            abc.Add("c", 3);
+            foreach (var item in abc)
+            {
+                Console.WriteLine(item.Key+" "+item.Value);
+            }
+        }
+        public void metho1()
+        {
+            //List<string> l1= new List<string>();
+            //l1.Add("pune");
+            //l1.Add("100");
+            //l1.Add("mumbai");
+
+            //foreach (var item in l1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("count="+l1.Count);
+            //Console.WriteLine("capacity="+l1.Capacity);
+            //List<string> l2 = new List<string>();
+            //l2.Add(l1[0]);
+            //Console.WriteLine("==============");
+            //foreach (var item in l1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Dictionary<string, int> RTOCity = new Dictionary<string, int>();
+            RTOCity.Add("pune", 12);
+            RTOCity.Add("solapur", 13);
+            RTOCity.Add("pimpri-Chinchwad", 14);
+
+            foreach (var item in RTOCity)
+            {
+                Console.WriteLine(item.Key);//pune solapur pimprichi
+                Console.WriteLine(item.Value);//12 13 14
+            }
+
+            List<string> cities1=new List<string>() { "pune","nagar","Nashik"};
+            List<string> cities2 = new List<string>() { "vijaypur", "Banglore" };
+            List<string> cities3 = new List<string>() { "patana"};
+
+            Dictionary<string, List<string>> states = new Dictionary<string, List<string>>();
+            states.Add("Maharashtra", cities1);
+            states.Add("Karnataka", cities2);
+            states.Add("Bihar", cities3);
+
+            string Fcity = "patana";
+
+            //bool flag = false;
+            //foreach (var item in states)
+            //{
+
+            //    foreach (var cty in item.Value)
+            //    {
+            //        if (string.Compare(Fcity,cty)==0)
+            //        {
+            //            flag = true;
+            //            break;
+            //        }
+            //    }
+            //    if (flag==true)
+            //    {
+            //        break;
+            //    }
+            //}
+            //if (flag==true)
+            //{
+            //    Console.WriteLine("found");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("not found");
+            //}
+        
+
+            string result = null;
+            foreach (var item in states)
+            {
+                 result = item.Value.FirstOrDefault(x => x == Fcity);
+                if (result!=null)
+                {
+                    Console.WriteLine("found");
+                    break;
+                }
+            }
+            if (result==null)
+            {
+                Console.WriteLine("Not found");
+            }
+            //foreach (var item in states)
+            //{
+            //    Console.WriteLine("state:"+item.Key);
+            //    foreach (var cty in item.Value)
+            //    {
+            //        Console.Write(" "+cty);
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
---
+++ GenericTry/Program.cs
@@ -0,0 +1,155 @@
+﻿using GenericTry.Generic_collection_example;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GenericTry
+{
+    class Assignment
+    {
+        public T FindMax1<T>(T[]array)
+        {
+            int a = -1;
+            T max = (dynamic)a;
+            try
+            {
+                max = array[0];//first element copied into max variable
+                foreach (dynamic i in array)
+                {
+                    if (i > max)
+                    {
+                        max = i;
+                    }
+                }
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+              return max;
+        }
+        public int FindMax(int[]array)
+        {
+            if (array.Length==0)
+            {
+                return -1;
+            }
+            int max = array[0];//first element copied into max variable
+            foreach (int i in array) 
+            {
+                if (i>max)
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+        public bool compare<T>(T a,T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            if (a1==b1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+   class class1
+    {
+        //public void swap(ref float a, ref float b)
+        //{
+        //    float c = a;
+        //    a = b;
+        //    b = c;
+        //}
+        //public void swap(ref int a,ref int b)
+        //{
+        //    int c = a;
+        //    a = b;
+        //    b= c;
+        //}
+
+        public void swap<T>(ref T a,ref T b)//generic method
+        {
+           T c = a;
+            a = b;
+            b = c;
+        }
+     
+    }
+   class ArithMeticOperation<T,W>// generic class
+    {
+        public T sum(T x,W y)
+        {
+            dynamic x1 = x;
+            dynamic y1 = y;
+            dynamic ans = x1 + y1;
+            return ans;
+        }
+        public T multiplication(T a,W b)
+        {
+            return (dynamic) a*(dynamic) b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int x = 100;
+            // int y = 200;
+            // Console.WriteLine("x="+x+" y="+y);
+
+            // class1 obj = new class1();
+            // char ch = 'C';
+            // obj.swap(ref x,ref y);//1 calling generic method
+            // Console.WriteLine("x=" + x + " y=" + y);
+
+
+            // float f1 = 3.14f;
+            // float f2 = 9.8f;
+            // Console.WriteLine(f1 + " " + f2);
+            // obj.swap(ref f1, ref f2);//2 calling generic method
+            // Console.WriteLine(f1 + " " + f2);
+
+            // string s1 = "pune";
+            // string s2 = "nagar";
+            // Console.WriteLine(s1 + " " + s2);
+            // obj.swap<string>(ref s1, ref s2);
+            // Console.WriteLine(s1+" "+s2);
+            // ////////////////////////////////////////////////
+            // ///
+
+            // ArithMeticOperation<float,int> amo = new ArithMeticOperation<float,int>();
+            // float ans1=amo.sum(100, 200);
+
+            //float ans2= amo.multiplication(100, 200);
+
+            // Console.WriteLine(ans1);//300
+            // Console.WriteLine(ans2);//20000
+
+            // ArithMeticOperation<double, int> amo2 = new ArithMeticOperation<double, int>();
+            // Console.WriteLine(amo2.sum(1.1f, 100));
+            // Console.WriteLine(amo2.multiplication(100, 2));
+
+            //ListDemo ld = new ListDemo();
+            //// ld.metho1();
+            //ld.SortedDictionaryDemo();
+            //Assignment a1 = new Assignment();
+            //bool result=a1.compare<float>(1.1f, 1.2f);
+            //Console.WriteLine(result);
+
+            double[] myArray = new double[] {1.1,5.5,6.6,2.2 };
+            Assignment a2 = new Assignment();
+            dynamic ans=a2.FindMax1(myArray);
+            if (ans!=-1)
+            {
+                Console.WriteLine(ans);
+            }
+                
+           
+        }
+    }
+}
